92a0eb8d4fb29278c2c664ab5294f907ff19e4cb
BallesAlieza <aliezaballes2@gmail.com>
2023-09-29 02:36:31 +0800

    added initial list

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args) {
+    
+        Student student = new Student("Alieza Balles", "IT21C", "learning is never done without error and defeat");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+       
+    }
+}
